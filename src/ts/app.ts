39567679f5d920b0f2eb9ae5b52ff00c5c109f42
Ekaterina Iachmeneva <iachmenevaev@mail.ru>
2021-04-22 10:39:34 +0300

    second commit

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -2,8 +2,6 @@ import Cart from './service/Cart';
 
 import Movie from './domain/Movie';
 const cart = new Cart;
-console.log(cart.items);
-
 cart.add(new Movie(
     1000,
     'Мстители',
@@ -16,4 +14,7 @@ cart.add(new Movie(
     137,
     'https://raw.githubusercontent.com/netology-code/ajs-homeworks/ajs8/typescript/pic/avengers.png',
     5))
-
+    console.log(cart.items);
+    console.log(typeof(cart.items));
+    const sute = cart.items.forEach((item) => console.log(typeof(item)));
+     
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -11,19 +11,16 @@ export default class Cart{
         return [...this._items];
     }
 sumTotal():number{
-   let result = this.items.reduce(function(sum,current){
-        return sum + current.price;
-    },0);
+   let result = this.items.reduce((sum,current) => sum +current.price,0);
     return result;
 }
 sumWithDiscount(discount:number): number { 
-    const resultWithOutdiscount = this.items.reduce(function(sum,current){
-        return sum + current.price;
-    },0);
+    const resultWithOutdiscount = this.sumTotal();
 return resultWithOutdiscount - resultWithOutdiscount*discount/100;
 }
 deleteItem(id:number):void {
-    const suteIndex = this.items.findIndex((item) => item.id === id);
+     console.log(this.items);
+     const suteIndex = this.items.findIndex((item:any) => item.id === id);
      this._items.splice(suteIndex,1);
 }
 }
